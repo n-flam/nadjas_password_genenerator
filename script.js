134844592a9b290ebbf47704d04b35042b414bce
Nadja Flaming <nadja.flam@gmx.net>
2020-11-24 15:22:34 +0800

    adding functions into js-script

--- script.js
+++ script.js
@@ -12,3 +12,24 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Function for UpperCase
+function getRandomUpperCase(){
+  return String.fromCharCode(Math.floor(Math.random()*26)+65);
+ }
+
+ // Function for LowerCase
+ function getRandomLowerCase(){
+  return String.fromCharCode(Math.floor(Math.random()*26)+97);
+}
+
+ // Function for Numbers
+ function getRandomNumber(){
+  return String.fromCharCode(Math.floor(Math.random()*10)+48);
+}
+
+ // Function for Symbols
+ function getRandomSymbol(){
+  var symbol = "!@#$%^&*(){}[]=<>/,.|~?";
+  return symbol[Math.floor(Math.random()*symbol.length)];
+}
